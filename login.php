bba3b711a7d78e6eb872e95b4b3d47bf433d9b14
github username <Joemiller065@gmail.com>
2020-01-16 14:00:49 -0500

    SEMI COLON WAS THE ONLY ISSUE WTFM8

--- login.php
+++ login.php
@@ -5,23 +5,28 @@
         <title>PHP Practice Login</title>
         <link rel="stylesheet" type="text/css" href="style.css">
         <?php 
-            $connection = mysqli_connect("localhost","user1","user1","loginPractice");
+            $connection = mysqli_connect("localhost","root","","loginPractice");
 
             if(isset($_POST['log'])){
                 $username = mysqli_real_escape_string($connect, $_POST['email']);
                 $passwd = mysqli_real_escape_string($connect, $_POST['password']);
                 
-
-                if($username != "" && $passwd != "") {
+                if($username!= "" && $passwd!= "") {
                     $sql = "SELECT id FROM login WHERE username='$username' and passwd='$passwd'";
                     $result = mysqli_query($connection, $sql);
-                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC)
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+                    $count = mysqli_num_rows($result);
+                    if($count==1){
+                        header("location: index.html");
+                    }
                 }
+                
             }
         ?>
     </head>
     <body>
-        <form class="form" action="login.php" method="post">
+        <form class="form" action="login.php" method="POST">
             <div class="container">
                 <label for="">Sign Up</label>
                 <input type="text" name="email" placeholder="Email" value="">
