03d2b31fd949cf820490c3c96a4f9ca2aca48a54
github username <Joemiller065@gmail.com>
2020-01-16 16:17:13 -0500

    finaly working had to do a bunch of learning about MySQL prvilidges but i got it working

--- login.php
+++ login.php
@@ -5,28 +5,35 @@
         <title>PHP Practice Login</title>
         <link rel="stylesheet" type="text/css" href="style.css">
         <?php 
+
             $connection = mysqli_connect("localhost","root","","loginPractice");
+            // Check connection
+            if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                exit();
+            }
 
             if(isset($_POST['log'])){
-                $username = mysqli_real_escape_string($connect, $_POST['email']);
-                $passwd = mysqli_real_escape_string($connect, $_POST['password']);
+                $email = mysqli_real_escape_string($connection, $_POST['email']);
+                $passwd = mysqli_real_escape_string($connection, $_POST['password']);
                 
-                if($username!= "" && $passwd!= "") {
-                    $sql = "SELECT id FROM login WHERE username='$username' and passwd='$passwd'";
+                if($email!= "" && $passwd!= "") {
+                    $sql = "SELECT id FROM login_prac WHERE username='$email' and passwd='$passwd' ";
                     $result = mysqli_query($connection, $sql);
                     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
                     $count = mysqli_num_rows($result);
                     if($count==1){
-                        header("location: index.html");
+                        header("Location:index.html");
                     }
                 }
+
                 
             }
         ?>
     </head>
     <body>
-        <form class="form" action="login.php" method="POST">
+        <form class="form" action="login.php" method="post">
             <div class="container">
                 <label for="">Sign Up</label>
                 <input type="text" name="email" placeholder="Email" value="">
