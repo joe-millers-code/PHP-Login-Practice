9af859ef909756a4deba29d491a104bac4e82acf
github username <Joemiller065@gmail.com>
2020-01-16 12:36:13 -0500

    basic set up complete

--- login.php
+++ login.php
@@ -3,11 +3,19 @@
     <head>
         <meta charset="UTF-8">
         <title>PHP Practice Login</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <?php 
+            
+        ?>
     </head>
     <body>
-        <form action="login.php" method="post">
+        <form class="form" action="login.php" method="post">
             <div class="container">
-            <label for="">Sign Up</label>
+                <label for="">Sign Up</label>
+                <input type="text" name="email" placeholder="Email" value="">
+                <input type="password" name="password" placeholder="Password" value="" >
+                <a href="#" >Forgot Password?</a>
+                <button type="submit" class="btn" name="log" >Login</button>
             </div>
         </form>
     </body>
