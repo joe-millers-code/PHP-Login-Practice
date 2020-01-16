e96d5eaa19ab8b8e46f4d9c2274ee99c6e5ba1ec
github username <Joemiller065@gmail.com>
2020-01-16 10:27:28 -0500

    initial commit

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Practice Login</title>
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <div class="container">
+            <label for="">Sign Up</label>
+            </div>
+        </form>
+    </body>
+</html>
